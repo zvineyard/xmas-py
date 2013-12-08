7a6fe4722bbe84241c66079e24f0512fb1e6201e
Zac Vineyard <zacvineyard@gmail.com>
2013-12-08 08:01:01 -0700

    Repo cleanup.

--- run.py
+++ run.py
@@ -1,19 +1,30 @@
 import os
 count = 0
 while (count < 1000):
+
+	# Turn on all the lights
 	os.system("gpio write 0 1;gpio write 1 1;gpio write 2 1;gpio write 3 1;gpio write 4 1;gpio write 5 1;gpio write 6 1;gpio write 7 1;")
 
+	# Set the volume
 	os.system("amixer set PCM 90%")
-	os.system("mpg123 vineyard2.mp3 > /dev/null")
 
+	# Play a welcome message
+	os.system("mpg123 welcome.mp3")
 
+	# Reset Volume
 	os.system("amixer set PCM 100%")
 
+	# Turn off all the lights
 	os.system("gpio write 0 0;gpio write 1 0;gpio write 2 0;gpio write 3 0;gpio write 4 0;gpio write 5 0;gpio write 6 0;gpio write 7 0;")
+
+	# Play songs
 	os.system("python coventry.py")
+
 	os.system("amixer set PCM 90%")
 	os.system("python wizards.py")
+
 	os.system("amixer set PCM 100%")
 	os.system("python bells.py")
+
 	os.system("python noel.py")
 	os.system("python wenceslas.py")
--- white_christmas.py
+++
@@ -1,6 +0,0 @@
-import os
-import time
-
-os.system("aplaymidi --port 14 bc_white_christmas.mid &")
-time.sleep(4.7)
-os.system("mpg123 white_christmas.mp3")
