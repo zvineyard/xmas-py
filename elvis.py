291ce058ee7dc7ba0a44908702b10d54fe89f4d5
Zac Vineyard <zacvineyard@gmail.com>
2015-11-28 23:43:40 +0000

    Time and playback adjustments

--- elvis.py
+++ elvis.py
@@ -1,7 +1,8 @@
 import os
 import time
 
-os.system("mpg123 /home/pi/pi-lightorgan-read-only/audio/elvis.mp3 & > /dev/null")
-time.sleep(0.6)
+os.system("aplaymidi --port 14 /home/pi/pi-lightorgan-read-only/audio/elvis.mid &")
 
-os.system("aplaymidi --port 14 /home/pi/pi-lightorgan-read-only/audio/elvis.mid > /dev/null")
+time.sleep(8.05)
+
+os.system("mpg123 /home/pi/pi-lightorgan-read-only/audio/elvis.mp3 & > /dev/null")
--- monsters.py
+++ monsters.py
@@ -1,7 +1,8 @@
 import os
 import time
 
-os.system("mpg123 /home/pi/pi-lightorgan-read-only/audio/monsters.mp3 & > /dev/null")
-time.sleep(0.6)
+os.system("aplaymidi --port 14 /home/pi/pi-lightorgan-read-only/audio/monsters.mid &")
 
-os.system("aplaymidi --port 14 /home/pi/pi-lightorgan-read-only/audio/monsters.mid > /dev/null")
+time.sleep(1.5)
+
+os.system("mpg123 /home/pi/pi-lightorgan-read-only/audio/monsters.mp3 & > /dev/null")
