6894a6ba818d07d223bdcfc6e673663169609f4e
hoblin <evgeny.gurin@gmail.com>
2016-12-05 11:56:15 +0200

    Follow up

--- monsters.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-os.system("aplaymidi --port 14 /home/pi/pi-lightorgan-read-only/audio/monsters.mid &")
-
-time.sleep(1.5)
-
-os.system("mpg123 /home/pi/pi-lightorgan-read-only/audio/monsters.mp3 & > /dev/null")
