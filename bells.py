5c863b9e0c0d4ba7b9ebbcbad1e0beee6f9f4170
hoblin <evgeny.gurin@gmail.com>
2016-12-05 12:29:26 +0200

    More songs & new way to play

--- bells.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-
-os.system("mpg123 -q /home/pi/pi-lightorgan-read-only/audio/carol-of-the-bells.mp3 &")
-time.sleep(0.55)
-
-os.system("aplaymidi --port 14 /home/pi/pi-lightorgan-read-only/audio/carol-of-the-bells.mid")
--- wenceslas.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-
-os.system("mpg123 -q /home/pi/pi-lightorgan-read-only/audio/wenceslas.mp3 &")
-time.sleep(0.6)
-
-os.system("aplaymidi --port 14 /home/pi/pi-lightorgan-read-only/audio/wenceslas.mid")
